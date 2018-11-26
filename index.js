ac5239b9a80b137ccce10933954c92ad94433aa7
lively-teacher-9822 <ccrive10@gmail.com>
2018-11-26 14:59:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 function happyHolidays() {
-  return "Happy holidays!"
+  return "Happy holidays!";
 }
 
 function happyHolidaysTo(name) {
-  return "Happy holidays, you!" 
-}
-
-function happyHolidayTo(holiday, name) {
-  return "Happy ${holiday}, ${name}"
-}
-
-function holidayCountdown(days, holiday) {
-  return "It's ${days} until ${holiday}"
+return ’Happy holidays, ${name}’
 }
   
+function happyHolidayTo(holiday, name) {
+  return ”Happy ${holiday} ${name}“ 
+}
+    
